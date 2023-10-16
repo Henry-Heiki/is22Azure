d98ebf5953bc9f74d4341c1abb3e2a64d91c6470
Henry-Heiki <115539753+Henry-Heiki@users.noreply.github.com>
2023-10-16 10:41:19 +0300

    Update index.php
    Katse 2

--- index.php
+++ index.php
@@ -1,3 +1,42 @@
-<html>
-  <H1>TestTestTestTest</H1>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Test Page</title>
+</head>
+<body>
+    <header>
+        <h1>TestTestTest</h1>
+    </header>
+
+    <nav>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </nav>
+
+    <main>
+        <section>
+            <h2>About Us</h2>
+            <p>This is a simple test page to demonstrate HTML and CSS.</p>
+        </section>
+
+        <section>
+            <h2>Services</h2>
+            <ul>
+                <li>Web Design</li>
+                <li>Graphic Design</li>
+                <li>SEO Optimization</li>
+            </ul>
+        </section>
+    </main>
+
+    <footer>
+        <p>Henry</p>
+    </footer>
+</body>
 </html>
